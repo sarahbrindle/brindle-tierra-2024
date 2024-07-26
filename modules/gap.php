d8344e53f46c9d4e54396baa247102a3a2783d3a
responsivedad <responsivedad@gmail.com>
2024-07-26 16:58:42 +0530

    Gap module added

---
+++ modules/gap.php
@@ -0,0 +1,10 @@
+<?php $module = $args['module']; ?>
+<!--module gap starts here-->
+<section class="module-gallery" data-aos="fade-up" data-aos-duration="400" data-aos-easing="cubic-bezier">
+  <div class="container-fluid" <?php if(isset($module['height'])){?> style="height:<?php echo $module['height'];?>px;" <?php }?>>
+
+            
+          
+  </div>
+</section>
+<!--module gallery ends here-->
